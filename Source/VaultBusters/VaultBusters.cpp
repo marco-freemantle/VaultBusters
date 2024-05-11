b02376bce507b380fa585d9ee9b8ee07e25ebc24
Marco Freemantle <marcofreemantle@gmail.com>
2024-05-11 16:54:18 +0100

    New engine version

---
+++ Source/VaultBusters/VaultBusters.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VaultBusters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VaultBusters, "VaultBusters" );
